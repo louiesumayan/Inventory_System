d0d28211545baa5cb41669053a293f39c804bc44
Louie <louiesumayab@gmail.com>
2025-05-30 01:01:20 +0800

    added jquery animation

--- javascript/script.js
+++ javascript/script.js
@@ -10,9 +10,7 @@ $(document).ready(function () {
     const eyeClose = 'images/Icons/Eye_close.png';
     $(this).attr('src', type === 'password' ? eyeClose : eyeOpen);
   });
-});
 
-$(document).ready(function () {
   // Animate h1 sliding in from left
   $('.content-container h1').animate(
     { left: '0', opacity: 1 },
@@ -22,9 +20,7 @@ $(document).ready(function () {
 
   // Animate p sliding in from right
   $('.content-container p').animate({ right: '0', opacity: 1 }, 900, 'swing');
-});
 
-$(document).ready(function () {
   $('.login-container').animate(
     { top: '0', opacity: 1 },
     1000, // 1 second duration
