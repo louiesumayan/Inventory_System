efead86f361de0d67f4a42461569a75a46586b2c
Louie <louiesumayab@gmail.com>
2025-05-29 22:37:27 +0800

    Inventory system using PHP vanilla

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php $page_title = 'Home Page' ?>
+<?php include './includes/header.php' ?>
+
+<div class="content-container">
+ <!-- content here -->
+</div>
+
+<?php include './includes/footer.php' ?>
