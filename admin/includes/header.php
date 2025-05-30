83a9c3a105959ffa03da019ae04cc4f967cf5245
Louie <louiesumayab@gmail.com>
2025-05-30 13:06:01 +0800

    initial page for admin w/ styles

---
+++ admin/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+ <link rel="stylesheet" href="./assets/css/<?php echo isset($page_style) ? $page_style : 'admin-styles.css' ?>">
+ <title><?php echo isset($page_title) ? $page_title : 'Admin Dashboard' ?></title>
+</head>
+
+<body>
