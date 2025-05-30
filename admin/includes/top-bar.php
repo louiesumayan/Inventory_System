d461f0a9f98985f93f5248166376a0081f9b4807
Louie <louiesumayab@gmail.com>
2025-05-30 21:24:31 +0800

    added top bar

---
+++ admin/includes/top-bar.php
@@ -0,0 +1,11 @@
+<section class="top-bar">
+ <h2><?php echo isset($currentPage) ? $currentPage : 'Admin Dashboard' ?>
+ </h2>
+ <a href="#" class="profile-link">
+  <img src="../images/Icons/Admin.png" alt="">
+  <div class="profile-info">
+   <p class="agent-name">brian</p>
+   <p class="profile-name">Brian Sumayan</p>
+  </div>
+ </a>
+</section>
