4147949b4f7906507343c0cbdb0033d0c542850e
Louie <louiesumayab@gmail.com>
2025-05-30 14:51:42 +0800

    organize layout for admin content

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -6,6 +6,11 @@
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
   integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+ <link rel="icon" href="../images/Logo.png" type="image/x-icon" />
+ <!-- fredoka font -->
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+ <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+ <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
 
  <link rel="stylesheet" href="./assets/css/<?php echo isset($page_style) ? $page_style : 'admin-styles.css' ?>">
  <title><?php echo isset($page_title) ? $page_title : 'Admin Dashboard' ?></title>
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,68 @@
+<section>
+ <div class="d-flex">
+  <!-- Sidebar -->
+  <aside class="sidebar d-flex flex-column">
+   <div class="logo-img">
+    <img class="logo-bm" src="../images/Logo.png" alt="">
+    <div class="logo">BM Digital Services</div>
+   </div>
+   <nav>
+
+    <h6>Home</h6>
+    <a href="#" class="active">
+     <div class="logo-home">
+      <img class="logo-bm" src="../images/Icons/Dashboard.png" alt="">
+      Dashboard
+     </div>
+    </a>
+
+    <h6>Sales</h6>
+
+    <a href="#">
+     <div class="logo-sales">
+      <img class="logo-bm" src="../images/Icons/Inventory.png" alt="">
+      Inventory
+     </div>
+    </a>
+
+    <a href="#">
+     <div class="logo-sales">
+      <img class="logo-bm" src="../images/Icons/Create_order.png" alt="">
+      Create order
+     </div>
+    </a>
+
+    <a href="#">
+     <div class="logo-sales">
+      <img class="logo-bm" src="../images/Icons/Payment.png" alt="">
+      Payment
+     </div>
+    </a>
+
+    <a href="#">
+     <div class="logo-sales">
+      <img class="logo-bm" src="../images/Icons/Sales.png" alt="">
+      Recent sales
+     </div>
+    </a>
+
+    <a href="#">
+     <div class="logo-sales">
+      <img class="logo-bm" src="../images/Icons/Admin.png" alt="">
+      Admin
+     </div>
+    </a>
+
+
+    <div class="settings-layout">
+     <h6>Settings</h6>
+     <a href="#">
+      <div class="logo-settings">
+       <img class="logo-bm" src="../images/Icons/Settings.png" alt="">
+       General
+      </div>
+     </a>
+    </div>
+
+   </nav>
+  </aside>
